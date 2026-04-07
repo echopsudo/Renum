f1f44a1160a5a819fae559940e8f157dba089671
echopsudo <radar010311@gmail.com>
2026-04-07 12:09:07 +0200

    Add port extraction for HTTP and SMB services
    Extract HTTP and SMB ports from fast_scan.txt and save to info directory.

--- scan.sh
+++ scan.sh
@@ -13,3 +13,14 @@ else
         echo "no modes provided, quitting!"
         quit 1
 fi
+
+wait
+
+HTTP_PORTS=$(grep -oE "[0-9]+/open/[^,]*http" scans/fast_scan.txt | cut -d/ -f1 )
+SMB_PORTS=$(grep -oE "[0-9]+/open/[^,]*microsoft-ds" scans/fast_scan.txt | cut -d/ -f1 )
+
+echo HTTP_PORTS > info/http_ports.txt
+echo SMB_PORTS > info/smb_ports.txt
+
+cat http_ports
+cat smb_ports
