29128d8aec76342c0d8b9270b17b464675618ff2
echopsudo <radar010311@gmail.com>
2026-04-07 12:26:44 +0200

    Change scan output file path to 'info/scan.txt'

--- scan.sh
+++ scan.sh
@@ -4,22 +4,23 @@ IP=$(cat info/ip.txt)
 MODE=$(cat info/modes.txt)
 
 if [[ $MODE == fast ]]; then
-        nmap -oG scans/scan.txt $IP
+        nmap -oG info/scan.txt $IP
 elif [[ $MODE == normal ]]; then
-        nmap -p- -oG scans/scan.txt $IP
+        nmap -p- -oG info/scan.txt $IP
 elif [[ $MODE == thorough ]]; then
-        nmap -sV -p- -oG scans/scan.txt $IP
+        nmap -sV -p- -oG info/scan.txt $IP
 else
         echo "no modes provided, quitting!"
+        echo "to fix this, re-run the renum script"
         quit 1
 fi
 
 wait
 
-HTTP_PORTS=$(grep -oE "[0-9]+/open/[^,]*http" scans/scan.txt | cut -d/ -f1 )
-SMB_PORTS=$(grep -oE "[0-9]+/open/[^,]*microsoft-ds" scans/scan.txt | cut -d/ -f1 )
-FTP_PORTS=$(grep -oE "[0-9]+/open/[^,]*ftp" scans/scan.txt | cut -d/ -f1 )
-SSH_PORTS=$(grep -oE "[0-9]+/open/[^,]*ssh" scans/scan.txt | cut -d/ -f1 )
+HTTP_PORTS=$(grep -oE "[0-9]+/open/[^,]*http" info/scan.txt | cut -d/ -f1 )
+SMB_PORTS=$(grep -oE "[0-9]+/open/[^,]*microsoft-ds" info/scan.txt | cut -d/ -f1 )
+FTP_PORTS=$(grep -oE "[0-9]+/open/[^,]*ftp" info/scan.txt | cut -d/ -f1 )
+SSH_PORTS=$(grep -oE "[0-9]+/open/[^,]*ssh" info/scan.txt | cut -d/ -f1 )
 
 echo $HTTP_PORTS > info/http_ports.txt
 echo $SMB_PORTS > info/smb_ports.txt
